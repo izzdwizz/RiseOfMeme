80d39a502981c9ff98dcdf7514505f0af91767c6
Izzdwizz <izuchukwu.uchendu@gmail.com>
2024-09-27 16:46:03 +0100

    footer text

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="relative bottom-2 w-full flex justify-center font-body text-xl md:text-3xl text-black/70">
+    <div className="relative bottom-2 w-full flex justify-center font-body text-lg md:text-3xl text-black/70">
       <strong className="font-[200] font-serif text-[1rem] md:text-[1.6rem] text-red-800 mr-2">
         © 2024
       </strong>{" "}
