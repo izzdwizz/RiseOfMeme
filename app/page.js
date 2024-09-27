d78811feac775dfbd31dd4843458e4f42b8e5b26
Izzdwizz <izuchukwu.uchendu@gmail.com>
2024-09-27 13:36:50 +0100

    padding update for responsiveness

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col wrapper p-8 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col wrapper p-4 md:p-8 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <Navbar />
       <LandingSection />
       <MemeSection />
