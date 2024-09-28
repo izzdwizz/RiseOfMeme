cf56dd77f1e51e4fa87b9c64e0e94f452ce06cd1
Izzdwizz <izuchukwu.uchendu@gmail.com>
2024-09-28 12:19:38 +0100

    update to text size

--- app/LandingSection.jsx
+++ app/LandingSection.jsx
@@ -99,7 +99,7 @@ const LandingSection = () => {
 
       {/* Animated Text */}
       {displayedText && (
-        <div className="animated-text absolute top-[50%] left-[50%] font-body font-[300] w-full justify-center text-center text-[0.75rem] md:text-lg text-black/70 animate-bounce transform -translate-x-1/2">
+        <div className="animated-text absolute top-[50%] left-[50%] font-body font-[300] w-full justify-center text-center text-[0.75rem] md:text-xl text-black/70 animate-bounce transform -translate-x-1/2">
           {displayedText}
         </div>
       )}
