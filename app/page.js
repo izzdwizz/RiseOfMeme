091cd158dcae638e2598e9083beb070ea3d56a7f
Izuchukwu Uchendu <71931658+izzdwizz@users.noreply.github.com>
2024-09-29 22:59:09 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import LandingSection from "./LandingSection";
 import Navbar from "./components/Navbar";
 import MemeSection from "./MemeSection";
 import Footer from "./components/Footer";
-
+//remember to add responsiveness
 export default function Home() {
   return (
     <div className="flex flex-col wrapper p-4 md:p-8 items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
